afc06ca468688545df71656a561b62d8f493489e
JorgeMm3 <144908159+JorgeMm3@users.noreply.github.com>
2025-02-06 12:29:46 +0100

    STATS - Version 3

--- ros2_ws/src/stats/src/mean.cpp
+++ ros2_ws/src/stats/src/mean.cpp
@@ -3,24 +3,23 @@
 #include <iostream>
 
 
-int sum;
+int message_count = 0;
 std::shared_ptr< rclcpp::Publisher<std_msgs::msg::Int32> > publisher;
 
 void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
 {
-    sum +=  msg->data;
+    message_count++;
     std_msgs::msg::Int32 out_msg;
-    out_msg.data = sum;
+    out_msg.data = message_count;
     publisher->publish(out_msg);
 }
 
 int main(int argc, char * argv[])
 {
-    sum = 0;
     rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("sum");
+    auto node = rclcpp::Node::make_shared("count_messages_node");
     auto subcription = node ->create_subscription<std_msgs::msg::Int32>("number", 10, topic_callback);
-    publisher = node->create_publisher<std_msgs::msg::Int32>("sum", 10);
+    publisher = node->create_publisher<std_msgs::msg::Int32>("message_count", 10);
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
