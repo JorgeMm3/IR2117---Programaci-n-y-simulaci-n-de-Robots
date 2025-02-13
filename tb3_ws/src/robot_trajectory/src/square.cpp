824f442b34affb5b0f70daac1f4f771b22e83605
JorgeMm3 <al415880@uji.es>
2025-02-13 12:52:51 +0100

    Move forward

--- tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/robot_trajectory/src/square.cpp
@@ -7,18 +7,26 @@ using namespace std::chrono_literals;
 int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("publisher");
+    auto node = rclcpp::Node::make_shared("square");
     auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+    node->declare_parameter("linear_speed", 0.1);
     geometry_msgs::msg::Twist message;
     rclcpp::WallRate loop_rate(500ms);
-
-    while (rclcpp::ok()) {
-        message.linear.x = 1.0;
-        message.linear.z = 1.0;
+   
+    double linear_speed = node->get_parameter("linear_speed").get_parameter_value().get<double>();
+    
+    int i=0, n=4;
+    while (rclcpp::ok() && i<n) {
+        message.linear.x = linear_speed;
+        message.linear.z = 0.0;
         publisher->publish(message);
         rclcpp::spin_some(node);
         loop_rate.sleep();
+        i++;
     }
+
+    message.linear.x = 0
+
     rclcpp::shutdown();
     return 0;
 }
