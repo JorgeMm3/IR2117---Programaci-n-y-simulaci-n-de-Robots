0235297224683aad1487a0aa04d35ae0349d32e7
JorgeMm3 <al415880@uji.es>
2025-02-24 09:22:34 +0100

    Reversion 3.0

--- Parcial/minmax/src/minmax/src/min.cpp
+++ Parcial/minmax/src/minmax/src/min.cpp
@@ -8,7 +8,7 @@ std::shared_ptr< rclcpp::Publisher<std_msgs::msg::Int32> > publisher;
 
 void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
 {
-    min +=  msg->data;
+    min =  msg->data;
     message_count++;
     std_msgs::msg::Int32 out_msg;
     out_msg.data = min;
@@ -19,7 +19,7 @@ int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
     auto node = rclcpp::Node::make_shared("min");
-    auto subcription = node ->create_subscription<std_msgs::msg::Int32>("number", 10, topic_callback);
+    auto subcription = node ->create_subscription<std_msgs::msg::Int32>("numbers", 10, topic_callback);
     publisher = node->create_publisher<std_msgs::msg::Int32>("min", 10);
     rclcpp::spin(node);
     rclcpp::shutdown();
