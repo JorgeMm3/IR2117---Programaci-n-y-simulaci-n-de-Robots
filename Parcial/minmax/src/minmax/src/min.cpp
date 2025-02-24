83709700af4177417171b5b06cd427d3672ba5bc
JorgeMm3 <al415880@uji.es>
2025-02-24 09:18:47 +0100

    Read an input message from the input topic, store the data in a variable , and displaye it

--- Parcial/minmax/src/minmax/src/min.cpp
+++ Parcial/minmax/src/minmax/src/min.cpp
@@ -2,12 +2,14 @@
 #include "std_msgs/msg/int32.hpp"
 #include <iostream>
 
-int min;
+int min = 0;
+int message_count = 0;
 std::shared_ptr< rclcpp::Publisher<std_msgs::msg::Int32> > publisher;
 
 void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
 {
     min +=  msg->data;
+    message_count++;
     std_msgs::msg::Int32 out_msg;
     out_msg.data = min;
     publisher->publish(out_msg);
@@ -15,11 +17,10 @@ void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
 
 int main(int argc, char * argv[])
 {
-    min = 0;
     rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("sum");
+    auto node = rclcpp::Node::make_shared("min");
     auto subcription = node ->create_subscription<std_msgs::msg::Int32>("number", 10, topic_callback);
-    publisher = node->create_publisher<std_msgs::msg::Int32>("sum", 10);
+    publisher = node->create_publisher<std_msgs::msg::Int32>("min", 10);
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
